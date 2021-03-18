f218ca0149016df078b559a389c0a5be55c70716
Harith Albadry <harith.albadry@gmail.com>
2021-03-18 00:11:17 +0000

    data trial

--- assests/javascript/script.js
+++ assests/javascript/script.js
@@ -9,23 +9,30 @@ var displayEl=$("display")
 // Functions used in the app
 
 // function to display time
-function displayTime() {
+function displayTime(){
     var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
     timeDisplayEl.text(rightNow);
     var hour =moment().format("HH")
     console.log(hour);
-    displayEvent();
-  }
+    for (i=1; i<3; i++) 
+    {
+     var rowEl= $(".body.main.childNodes[i].getAttribute('data-time')");
+    //   var rowData= rowEl.attr("data-time");
+    //  console.log(document.querySelector(".body.main.childNodes[i])};
+    }
+    }
+    
+   
   setInterval(displayTime, 1000);
 
 
-// save button function
-saveEl.on("click", function(event){
-    var element = event.target;
-    var test=element.getAttribute("data-id");
-    console.log(test);
+// // save button function
+// saveEl.on("click", function(event){
+//     var element = event.target;
+//     var test=element.getAttribute("data-id");
+//     console.log(test);
 
-})
+// })
 
 function displayEvent (){
 var eventText=eventEl.value.trim();
