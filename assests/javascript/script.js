5ba05a2d1b2a6f1b69d4c1dc7a056d092af0e7de
Harith Albadry <harith.albadry@gmail.com>
2021-03-19 20:18:40 +0000

    improved colour scheme for past, present and future

--- assests/javascript/script.js
+++ assests/javascript/script.js
@@ -1,11 +1,5 @@
 var timeDisplayEl = $("#currentDay")
 var timeBlockEl = document.querySelectorAll(".row")
-// var eventEl9 = $("#event_input9")
-// var eventEl10 = $("#event_input10")
-// var eventEl11 = $("#event_input11")
-// var saveEl9 = $("#save_button9")
-// var saveEl10 = $("#save_button10")
-// var saveEl11 = $("#save_button11")
 
 
 // Button function
@@ -44,20 +38,21 @@ function displayTime() {
 		var backEL = parseInt(timeBlockEl[i].getAttribute("id"))
 		console.log(typeof backEL)
 		if (backEL < hour) {
-			timeBlockEl[i].setAttribute(
-				"style",
-				"background-color: yellow"
-			)
-		} else if (backEL == hour) {
+			timeBlockEl[i].setAttribute (
+				"style", "background-color: #60b4a4")
+      } else if (backEL == hour) {
+
+        
 			console.log("current hour")
 			timeBlockEl[i].setAttribute(
 				"style",
-				"background-color: purple"
+				"background-color: #94d8ca"
 			)
 		} else if (backEL > hour) {
+      
 			timeBlockEl[i].setAttribute(
 				"style",
-				"background-color: red"
+				"background-color: #9eab91"
 			)
 		}
 	}
@@ -77,46 +72,3 @@ function displayTime() {
 
 setInterval(displayTime, 1000)
 
-
-// function submit9(event){
-//     event.preventDefault();
-//     var nameOfEvent=eventEl9.value.trim();
-//     console.log(nameOfEvent);
-//     localStorage.setItem("levent9",JSON.stringify(nameOfEvent));
-
-// }
-
-// function submit10(event){
-//     event.preventDefault();
-//     var nameOfEvent=eventEl10.value.trim();
-//     console.log(nameOfEvent);
-//     localStorage.setItem("levent10",JSON.stringify(nameOfEvent));
-
-// }
-
-// function submit11(event){
-//     event.preventDefault();
-//     var nameOfEvent=eventEl11.value.trim();
-//     console.log(nameOfEvent);
-//     localStorage.setItem("levent11",JSON.stringify(nameOfEvent));
-
-// }
-
-// function write9(){
-//     var writeEvent=JSON.parse(localStorage.getItem("levent9"));
-//     text9.text=(writeEvent);
-// }
-
-// function write10(){
-//     var writeEvent=JSON.parse(localStorage.getItem("levent10"));
-//     text10.text=(writeEvent);
-// }
-
-// function write11(){
-//     var writeEvent=JSON.parse(localStorage.getItem("levent11"));
-//     text11.text=(writeEvent);
-// }
-
-// saveEl9.on("click",submit9);
-// saveEl10.on("click",submit10);
-// saveEl11.on("click",submit11);
